1c31f36421fb20eff768980b7c1b39b37ee574b3
janis50000 <jan.janetzko@gmail.com>
2022-03-01 07:35:20 +0100

    blink.py

--- blink.py
+++ blink.py
@@ -3,10 +3,11 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.OUT)
-GPIO.output(23, GPIO.HIGH)
-time.sleep(0.5)
-GPIO.output(23, GPIO.LOW)
-time.sleep(0.5)
+for i in range(5):
+    GPIO.output(23, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(23, GPIO.LOW)
+    time.sleep(0.5)
 
 
 
