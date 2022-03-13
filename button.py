7ff7914a2db25c8c4e002dcde7ebaf7c72e38557
janis50000 <jan.janetzko@gmail.com>
2022-03-13 11:30:16 +0100

    reverse for relais

--- button.py
+++ button.py
@@ -9,10 +9,12 @@ GPIO.setup(24, GPIO.IN)
 while True:
     if GPIO.input(24) == 0:
         # Ausschalten
-        GPIO.output(23, GPIO.LOW)
+        #GPIO.output(23, GPIO.LOW)
+        PIO.output(23, GPIO.HIGH)
     else:
         # Einschalten
-        GPIO.output(23, GPIO.HIGH)
+        #GPIO.output(23, GPIO.HIGH)
+        GPIO.output(23, GPIO.LOW)
 
 '''
 for i in range(5):
